f966ffb861e8bae74576aa7517515a8d4b6b56b6
System Administrator <root@souqs-MacBook-Pro-3.local>
2017-07-08 19:08:10 +0300

    Fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -60,7 +60,7 @@ class HomeController extends ExpediaController
             $resultHotels = $data->offers->Hotel;
         }
 
-
+print_r($data->offers);
 
         return view('pages.one',['hotels'=> $resultHotels, 'searchDetails' => true]);
     }
